c94b098fa088843e2291478e1ca501fff954f0a6
weblebb <115215395+weblebb@users.noreply.github.com>
2023-07-06 19:05:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-# Python program to check if the input number is odd or even.
-# A number is even if division by 2 gives a remainder of 0.
-# If the remainder is 1, it is an odd number.
-
-num = int(input("Enter a number: "))
-if (num % 3) == 0:
-   print("{0} is Even".format(num))
+# Python program to check if the input number is positive or negative.
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
 else:
-   print("{0} is Odd".format(num))
+   print("Negative number")
