21c8a83d69ac85d3b48226cf57494298fd4c31d6
weblebb <115215395+weblebb@users.noreply.github.com>
2023-07-06 19:48:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Python program to check if the input number is positive or negative.
 num = float(input("Enter a number: "))
-if num > 0:
+if num < 0:
    print("Positive number")
 elif num == 0:
    print("Zero")
